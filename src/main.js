03f9e89632c09c3e4a6d3bd55662b6e082142972
daw2-sorroche23 <sorrocheperezzeus@fpllefia.com>
2023-02-24 16:10:36 +0100

    inicial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import { footer } from "./componentes/footer";
+import { header } from "./componentes/header";
+import { home } from "./vistas/home";
+
+document.querySelector('main').innerHTML = home.template;
+document.querySelector('header').innerHTML = header.template;
+document.querySelector('footer').innerHTML = footer.template;
